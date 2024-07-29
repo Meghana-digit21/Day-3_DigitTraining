df504674d21afb42ce9015eee3fe2f1eab3c4703
Barla Sai <Barla.Sai@godigit.com>
2024-07-29 12:07:16 +0530

    [Meghana]: Adding Delete contact method in AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -1,3 +1,4 @@
+
 package com.addressbook;
 
 import java.util.HashMap;
@@ -90,6 +91,26 @@ public class AddressBook {
         }
         else{
             System.out.println("User Not Found !!");
+        }
+    }
+
+    public static void deletecontact(HashMap<String, Contact> memberinfo){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Firstname: ");
+        String first_name = sc.nextLine();
+
+        System.out.println("Enter Lastname: ");
+        String last_name = sc.nextLine();
+
+        if(memberinfo.containsKey(first_name+" "+last_name)){
+            memberinfo.remove(first_name+" "+last_name);
+            System.out.println("User Deleted !!!");
+            System.out.println("");
+        }
+        else{
+            System.out.println("User Not Found !!!");
+            System.out.println("");
         }
     }
 }
